97821a82685219b4466206a3d799772aa5468c52
gauravkaushikcode <gori.kaushik@gmail.com>
2021-03-08 16:54:48 +0200

    adding HOC

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "actions";
+import requireAuth from "./requireAuth";
 
 class CommentBox extends Component {
   state = { comment: "" };
@@ -33,4 +34,4 @@ class CommentBox extends Component {
   }
 }
 
-export default connect(null, actions)(CommentBox);
+export default connect(null, actions)(requireAuth(CommentBox));
--- testing/src/reducers/commentsReducer.js
+++ testing/src/reducers/commentsReducer.js
@@ -1,13 +1,14 @@
 import { SAVE_COMMENT, FETCH_COMMENTS } from "actions/types";
 
-export default (state = [], action) => {
+export default function (state = [], action) {
   switch (action.type) {
     case SAVE_COMMENT:
       return [...state, action.payload];
     case FETCH_COMMENTS:
+      debugger;
       const comments = action.payload.data.map((comment) => comment.name);
       return [...state, ...comments];
     default:
       return state;
   }
-};
+}
